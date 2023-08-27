23b69017b8de51891ff78adf3cc5987c88e958d8
Arter Tendean <arter.tendean.07@gmail.com>
2023-08-28 01:30:01 +0800

    Fix formatter error

--- pacuan_kuda/apps.py
+++ pacuan_kuda/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PacuanKudaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pacuan_kuda'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pacuan_kuda"
