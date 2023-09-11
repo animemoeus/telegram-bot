f7cbf4e364cd3172a9ebb7b1569082e83dc4b70a
Arter Tendean <arter.tendean.07@gmail.com>
2023-09-11 19:50:49 +0800

    Update admin

--- twitter_video_downloader/admin.py
+++ twitter_video_downloader/admin.py
@@ -4,7 +4,16 @@ from .models import TelegramUser
 
 
 class TelegramUserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["first_name", "last_name", "username", "request_count"]
+    readonly_fields = [
+        "user_id",
+        "first_name",
+        "last_name",
+        "username",
+        "request_count",
+        "created_at",
+        "updated_at",
+    ]
 
 
 admin.site.register(TelegramUser, TelegramUserAdmin)
