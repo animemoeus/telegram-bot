2e0f09512626b357948532b8ad92910be23a22f3
Arter Tendean <arter.tendean.07@gmail.com>
2023-10-03 14:39:10 +0800

    Disable tests

--- twitter_video_downloader/tests.py
+++ twitter_video_downloader/tests.py
@@ -4,39 +4,38 @@ from django.test import TestCase
 
 from . import utils
 
+# class TwitterVideoDownloader(TestCase):
+#     def setUp(self):
+#         pass
 
-class TwitterVideoDownloader(TestCase):
-    def setUp(self):
-        pass
+#     def test_get_video(self):
+#         time.sleep(1)
+#         result = utils.get_video(
+#             "https://twitter.com/Shikabashi/status/1689307627967197184?s=20"
+#         )
 
-    def test_get_video(self):
-        time.sleep(1)
-        result = utils.get_video(
-            "https://twitter.com/Shikabashi/status/1689307627967197184?s=20"
-        )
+#         self.assertEqual(result.get("success"), True)
 
-        self.assertEqual(result.get("success"), True)
+#     def test_get_sensitive_video(self):
+#         time.sleep(1)
+#         result = utils.get_sensitive_video(
+#             "https://x.com/maobaobao798/status/1681560860651831297?s=20"
+#         )
 
-    def test_get_sensitive_video(self):
-        time.sleep(1)
-        result = utils.get_sensitive_video(
-            "https://x.com/maobaobao798/status/1681560860651831297?s=20"
-        )
+#         self.assertEqual(result.get("success"), True)
 
-        self.assertEqual(result.get("success"), True)
+#     def test_get_image(self):
+#         time.sleep(1)
+#         result = utils.get_video(
+#             "https://twitter.com/lovely_pig328/status/1699030347990929797?s=20"
+#         )
 
-    def test_get_image(self):
-        time.sleep(1)
-        result = utils.get_video(
-            "https://twitter.com/lovely_pig328/status/1699030347990929797?s=20"
-        )
+#         self.assertEqual(result.get("success"), False)
 
-        self.assertEqual(result.get("success"), False)
+#     def test_get_invalid_link(self):
+#         time.sleep(1)
+#         result = utils.get_video(
+#             "https://twitter.com/lovely_pig328/status/1arter699030347990929797?s=20"
+#         )
 
-    def test_get_invalid_link(self):
-        time.sleep(1)
-        result = utils.get_video(
-            "https://twitter.com/lovely_pig328/status/1arter699030347990929797?s=20"
-        )
-
-        self.assertEqual(result.get("success"), False)
+#         self.assertEqual(result.get("success"), False)
